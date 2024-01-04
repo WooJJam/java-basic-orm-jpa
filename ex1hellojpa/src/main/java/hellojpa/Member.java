1637fb90f17be4fcb08d9a310be7fe20a1886456
WooJJam <jaemin5548@naver.com>
2024-01-04 19:54:50 +0900

    [Section 2] init Jpa Project

---
+++ ex1hellojpa/src/main/java/hellojpa/Member.java
@@ -0,0 +1,30 @@
+package hellojpa;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user") // query가 user라는 테이블에 insert
+public class Member {
+
+    @Id
+    private Long id;
+    private String name;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
