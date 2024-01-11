0bcdf4d8af277fe76cef4b6bf6a08320eb0fc4a0
WooJJam <jaemin5548@naver.com>
2024-01-12 01:03:26 +0900

    [Section 8] 프록시와 연관관계 정리 - 프록시

--- ex1hellojpa/src/main/java/hellojpa/JpaMain.java
+++ ex1hellojpa/src/main/java/hellojpa/JpaMain.java
@@ -1,13 +1,6 @@
 package hellojpa;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.EntityTransaction;
-import jakarta.persistence.Persistence;
-
-import java.awt.*;
-import java.time.LocalDateTime;
-import java.util.List;
+import jakarta.persistence.*;
 
 public class JpaMain {
 
@@ -18,33 +11,36 @@ public class JpaMain {
         tx.begin();
 
         try {
-<<<<<<< HEAD
-<<<<<<< HEAD
-////            Member findMember = em.find(Member.class, 2);
-//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-//                    .setFirstResult(1) // 1 번부터
-//                    .setMaxResults(8) // 8개
-//                    .getResultList();
-////            findMember.setName("HelloJPA");
-//            for (Member member: result) {
-////                System.out.println("member.getName() = " + member.getName());
-//            }
-            Team team = new Team();
-            team.setName("TeamA");
-            em.persist(team);
 
-=======
->>>>>>> section6
-            Member member = new Member();
-            member.setUsername("member1");
-=======
->>>>>>> section7
+            Member member1 = new Member();
+            member1.setUsername("member1");
+            em.persist(member1);
 
-            Member member = new Member();
-            member.setUsername("user1");
-            member.setCreatedBy("kim");
-            member.setCreatedDate(LocalDateTime.now());
-            em.persist(member);
+            Member member2 = new Member();
+            member2.setUsername("member2");
+            em.persist(member2);
+
+            em.flush();
+            em.clear();
+            
+            Member m1 = em.find(Member.class, member1.getId());
+            Member m2 = em.getReference(Member.class, member2.getId());
+            System.out.println("m1, m2 = " + (m1 instanceof Member));
+            System.out.println("m1, m2 = " + (m2 instanceof Member));
+
+            Member m3 = em.find(Member.class, member1.getId());
+            System.out.println("m3.getClass() = " + m3.getClass());
+
+            em.flush();
+            em.clear();
+            
+            Member m4 = em.getReference(Member.class, member1.getId());
+            System.out.println("m4.getClass() = " + m4.getClass());
+            System.out.println("m4.getId() = " + m4.getId());
+            System.out.println("isLoaded = " + emf.getPersistenceUnitUtil().isLoaded(m4));
+//            Member findMember = em.getReference(Member.class, member.getId());
+//            System.out.println(findMember.getId());
+//            System.out.println("findMember.getUsername() = " + findMember.getUsername());
 
             tx.commit();
         } catch (Exception e) {
