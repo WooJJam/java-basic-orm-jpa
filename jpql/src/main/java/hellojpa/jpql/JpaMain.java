a0f9f9013e486eb3b17ec3c64ee0a15a8c63869b
WooJJam <jaemin5548@naver.com>
2024-01-15 20:11:49 +0900

    [Section 10] 객체지향 쿼리 언어1 - 프로젝션(SELECT)

--- jpql/src/main/java/hellojpa/jpql/JpaMain.java
+++ jpql/src/main/java/hellojpa/jpql/JpaMain.java
@@ -12,31 +12,50 @@ public class JpaMain {
         tx.begin();
 
         try {
+//            Member member = new Member();
+//            member.setUsername("member1");
+//            member.setAge(10);
+//            em.persist(member);
+//
+//            // TypeQuery: 반환 타입이 명확할 때
+//            // Query: 반환 타입이 명확하지 않을 때
+//            TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
+////            TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class);
+////            Query query3 = em.createQuery("select m.username, m.age from Member m");
+//
+//            List<Member> resultList = query1.getResultList();
+//            for (Member member1 : resultList) {
+//                System.out.println("member1 = " + member1);
+//            }
+//
+//            TypedQuery<Member> query2 = em.createQuery("select m from Member m where m.id = 1", Member.class);
+//            // SingleResult의 경우 결과가 없으면: NoResultException
+//            //                    결과가 둘 이상이면: NonUniqueResultException
+//            Member result = query2.getSingleResult();
+//
+//            TypedQuery<Member> query3 = em.createQuery("select m from Member m where m.username = :username", Member.class);
+//            query3.setParameter("username", "member1");
+//            Member singleResult = query3.getSingleResult();
+//            System.out.println("singleResult = " + singleResult.getUsername());
+
             Member member = new Member();
             member.setUsername("member1");
             member.setAge(10);
             em.persist(member);
 
-            // TypeQuery: 반환 타입이 명확할 때
-            // Query: 반환 타입이 명확하지 않을 때
-            TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
-//            TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class);
-//            Query query3 = em.createQuery("select m.username, m.age from Member m");
+            em.flush();
+            em.clear();
 
-            List<Member> resultList = query1.getResultList();
-            for (Member member1 : resultList) {
-                System.out.println("member1 = " + member1);
-            }
+            List<Member> result = em.createQuery("select m from Member m", Member.class).getResultList();
+            Member member1 = result.get(0);
+            member1.setAge(20);
+            em.createQuery("select t from Member m join m.team t", Team.class).getResultList();
+            em.createQuery("select distinct m.username, m.age from Member m").getResultList();
+            List<MemberDTO> result2 = em.createQuery("select new hellojpa.jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class).getResultList();
 
-            TypedQuery<Member> query2 = em.createQuery("select m from Member m where m.id = 1", Member.class);
-            // SingleResult의 경우 결과가 없으면: NoResultException
-            //                    결과가 둘 이상이면: NonUniqueResultException
-            Member result = query2.getSingleResult();
-
-            TypedQuery<Member> query3 = em.createQuery("select m from Member m where m.username = :username", Member.class);
-            query3.setParameter("username", "member1");
-            Member singleResult = query3.getSingleResult();
-            System.out.println("singleResult = " + singleResult.getUsername());
+            MemberDTO memberDTO = result2.get(0);
+            System.out.println("memberDTO. = " + memberDTO.getUsername());
+            System.out.println("memberDTO. = " + memberDTO.getAge());
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
